e131b63d69716b0f124eabc2761860abcdbe5f21
Muhbashir2005 <onibashirmuhbashir@gmail.com>
2022-12-04 09:05:00 +0100

    Final-commit

--- StartApp.js
+++ StartApp.js
@@ -1,7 +1,9 @@
 console.log("Decoding, currently not available");
-var data = "I am now a boy in university ";
+var data = "AfricanJournal";
+// var dedata ="foiraaAJrucnnl"
 var f = Encode(data);
-console.log(f);
+// console.log("Encoded data:"+ f);
+// console.log(Decode(dedata));
 function Encode(data){
   data[0];
 	var a = data.substring(0, data.length/2);
@@ -21,21 +23,20 @@ function Encode(data){
 	}
   console.log(l + k);
 }  
-// function Decode(data){
-//   var c = data.substring(0, data.length/2 - 1);
-// 	var d = data.substring(data.length/2 -1, data.length);
-//   var h = "";
-//   var g = "";
-//   for (let j = 0; j < c.length; j++) {
-//     if (j%2 != 0) {
-//       h += d[j];
-//       h += c[j];
-//     }
-//     if (j%2 == 0) {
-//       g += d[j];
-//       g += c[j];
-//     }
-//   }
-//   console.log(h);
-//   console.log(g);
-// }
+function Decode(data){
+  var c = data.substring(0, data.length/2 - 1);
+	var d = data.substring(data.length/2 -1, data.length);
+  var h = "";
+  var g = "";
+  for (let j = 0; j < c.length; j++) {
+    if (j%2 != 0) {
+      h += d[j];
+      h += c[j];
+    }
+    if (j%2 == 0) {
+      g += d[j];
+      g += c[j];
+    }
+  }
+  return g + h;
+}
